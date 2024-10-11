6fba4419225b6c436a543bd3156238eb63a0dde6
Xavier Mitault <xavier.mitault@epitech.eu>
2024-10-11 13:15:51 +0100

    Fix paq repo

--- .github/workflows/generate_metadata.py
+++ .github/workflows/generate_metadata.py
@@ -8,7 +8,7 @@ from hashlib import md5
 
 PACKAGES_FILE = "/tmp/paq-packages.toml"
 with open(PACKAGES_FILE, "w") as f:
-    f.write("name = \"paq\"\n\n")
+    f.write("name = \"paq-packages1\"\n\n")
 
 LOG_FILE = "/tmp/paq-packages.log"
 with open(LOG_FILE, "w") as f:
